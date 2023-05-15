903f5872a32a3493ec469b9eb0f33b5a470ead03
angelina <133657155+angelinas07@users.noreply.github.com>
2023-05-15 17:08:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button4 = findViewById(R.id.button4);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь нужно выполнить переход на следующую активность:
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь нужно выполнить переход на следующую активность:
+                Intent intent = new Intent(SecondActivity.this, Difficulty.class);
+                startActivity(intent);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь нужно выполнить переход на следующую активность:
+                Intent intent = new Intent(SecondActivity.this, Difficulty.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
